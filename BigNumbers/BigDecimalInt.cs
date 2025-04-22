40d7c45e3d2a3cd61553dadf1a8dafcca93fefea
Alexander <84857900+4z0t@users.noreply.github.com>
2025-04-22 17:12:35 +0300

    add AdsAdd and AbsSub

--- BigNumbers/BigDecimalInt.cs
+++ BigNumbers/BigDecimalInt.cs
@@ -40,6 +40,103 @@ namespace BigNumbers
             _contrainer.ResetLength();
         }
 
+        public BigDecimalInt(BigDecimalInt other)
+        {
+            _contrainer = new BigIntContrainer<uint>(other._contrainer);
+        }
+
+        private BigDecimalInt(int capacity, Sign sign)
+        {
+            _contrainer = new BigIntContrainer<uint>(capacity, sign);
+        }
+
+        private BigDecimalInt AbsAdd(BigDecimalInt other, Sign sign)
+        {
+            int len1 = _contrainer.Length;
+            int len2 = other._contrainer.Length;
+
+            BigDecimalInt result = new BigDecimalInt(Math.Max(len1, len2) + 1, sign);
+            BigDecimalInt large = len1 > len2 ? this : other;
+            BigDecimalInt small = len1 > len2 ? other : this;
+
+            for (int i = 0; i < large._contrainer.Length; i++)
+            {
+                uint n1 = large._contrainer.Value[i];
+
+                if (i < small._contrainer.Length)
+                {
+                    uint n2 = small._contrainer.Value[i];
+                    uint s = n1 + n2;
+
+                    if (s == 0)
+                        continue;
+
+                    DivResult d = new(s, Base);
+
+                    result._contrainer.Value[i] += d.reminder;
+                    result._contrainer.Value[i + 1] += d.quotient;
+
+                    d = new DivResult(result._contrainer.Value[i], Base);
+
+                    result._contrainer.Value[i] = d.reminder;
+                    result._contrainer.Value[i + 1] += d.quotient;
+                }
+                else
+                {
+                    if (n1 == 0)
+                        continue;
+
+
+                    DivResult d = new(result._contrainer.Value[i] + n1, Base);
+
+                    result._contrainer.Value[i] = d.reminder;
+                    result._contrainer.Value[i + 1] += d.quotient;
+                }
+            }
+
+
+            result._contrainer.ResetLength();
+            return result;
+        }
+
+        private BigDecimalInt AbsSub(BigDecimalInt other, Sign sign)
+        {
+            int len1 = _contrainer.Length;
+            int len2 = other._contrainer.Length;
+
+            int c = AbsCompare(this, other);
+            if (c == 0)
+            {
+                return new BigDecimalInt();
+            }
+
+            BigDecimalInt large = c > 0 ? this : other;
+            BigDecimalInt small = c > 0 ? other : this;
+            BigDecimalInt result = new(large);
+
+            for (int i = 0; i < small._contrainer.Length; i++)
+                if (result._contrainer.Value[i] >= small._contrainer.Value[i])
+                {
+                    result._contrainer.Value[i] -= small._contrainer.Value[i];
+                }
+                else
+                {
+                    int j = i + 1;
+                    while (result._contrainer.Value[j] == 0)
+                    {
+                        result._contrainer.Value[j] = Base - 1;
+                        j++;
+                        if (j == small._contrainer.Length)
+                            break;
+                    }
+                    result._contrainer.Value[j] -= 1;
+                    result._contrainer.Value[i] += Base - small._contrainer.Value[i];
+                }
+
+            result._contrainer.ResetLength();
+            return result;
+        }
+
         private static int AbsCompare(BigDecimalInt left, BigDecimalInt right)
             => BigIntContrainer<uint>.AbsCompare(left._contrainer, right._contrainer);
 
@@ -50,7 +147,7 @@ namespace BigNumbers
             ArgumentNullException.ThrowIfNull(other);
 
             Sign r = other._contrainer.Sign;
-            Sign l = this._contrainer.Sign;
+            Sign l = _contrainer.Sign;
 
             if (l == Sign.Positive && r == Sign.Negative)
                 return 1;
